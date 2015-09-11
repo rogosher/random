4520a1964f0101adf7a567b31f1b39df1e759c62
rogosher <shogerr@onid.oregonstate.edu>
2015-09-10 21:41:36 -0700

    Update and rename mysql_bootstrap.sql to rails_bootstrap.sql

--- scripts/rails/rails_bootstrap.sql
+++ scripts/rails/rails_bootstrap.sql
@@ -1,54 +1,46 @@
---
---  Author: rogosher
---
---  Description: Ruby on Rails mysql setup. Script adds development, production,
---  and test databases to mysql server. Add a basic and admin user with related
---  permissions.
---
---  Executing:
---      `$ mysql -u root -p < mysql_bootstrap.sh`
---
+/* 
+  Author: rogosher
 
--- Uncomment if not setting variables via command line.
--- Example:
---     mysql -u root -p -e
---       "set @dbname='';set @username='';set @user_password='';set @admin_password='';`cat mysql_bootstrap.sql`"
+  Description: Ruby on Rails mysql setup. Script adds development, production,
+  and test databases to mysql server. Add a basic and admin user with related
+  permissions.
+
+  Executing:
+      `$ mysql -u root -p < mysql_bootstrap.sh`
+*/
+
+-- Uncomment if not setting via command line.
+-- `mysql -u root -p -e"set @dbname=''; set @username=''; set
+-- @user_password=''; set @admin_password='';" < mysql_bootstrap.sql`
 
 -- SET @dbname = 'mydb';
 -- SET @username = 'mydbuser';
 -- SET @user_password = 'passuser';
 -- SET @admin_password = 'passadmin';
 
-------------------------
--- *Create Databases* --
-------------------------
-
 -- create development database
-SET @s = CONCAT('CREATE DATABASE IF NOT EXISTS ', @dbname, '_development');
+SET @s = CONCAT('CREATE DATABASE IF NOT EXISTS ', @dbname, '_development DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci');
 PREPARE stmt_create FROM @s;
 EXECUTE stmt_create;
 DEALLOCATE PREPARE stmt_create;
 
 -- create production database
-SET @s = CONCAT('CREATE DATABASE IF NOT EXISTS ', @dbname, '_production');
+SET @s = CONCAT('CREATE DATABASE IF NOT EXISTS ', @dbname, '_production DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci');
 PREPARE stmt_create FROM @s;
 EXECUTE stmt_create;
 DEALLOCATE PREPARE stmt_create;
 
 -- create test database
-SET @s = CONCAT('CREATE DATABASE IF NOT EXISTS ', @dbname, '_test');
+SET @s = CONCAT('CREATE DATABASE IF NOT EXISTS ', @dbname, '_test DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci');
 PREPARE stmt_create FROM @s;
 EXECUTE stmt_create;
 DEALLOCATE PREPARE stmt_create;
 
-----------------------------------------
--- *Create Users and Set Permissions* --
-----------------------------------------
+-- Create or grant user @username and @username_admin permission on created
+-- databases.
 
-----------------------
--- User permissions --
-----------------------
-
+-- Set user permissions
+--
 -- production database
 SET @user_perms = "Select,Insert,Update,Delete,Lock Tables";
 SET @s = CONCAT("GRANT ", @user_perms," ON ", @dbname, "_production.* TO '",
@@ -73,10 +65,8 @@ PREPARE stmt_user FROM @s;
 EXECUTE stmt_user;
 DEALLOCATE PREPARE stmt_user;
 
------------------------
--- Admin permissions --
------------------------
-
+-- Set admin permissions
+--
 -- production
 SET @user_perms = "Select,Insert,Update,Delete,Create,Drop,Index,Alter,Lock Tables";
 SET @s = CONCAT("GRANT ", @user_perms," ON ", @dbname, "_production.* TO '",
