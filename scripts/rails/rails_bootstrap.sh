fad3c76628e80a3c2c05698a35230e63a0fdc8a0
rogosher <shogerr@onid.oregonstate.edu>
2015-09-12 10:33:39 -0700

    Update rails_bootstrap.sh

--- scripts/rails/rails_bootstrap.sh
+++ scripts/rails/rails_bootstrap.sh
@@ -1,8 +1,8 @@
-#USER_PASSWORD=$1
-#ADMIN_PASSWORD=$2
+# Variable wrapper for setting up MySQL tables for Rails 4
+# author: rogosher
 
-# \function gen_pass
-# \description generate a random password of 30 characters
+# gen_pass
+#   generate a random password of 30 characters
 function gen_pass()
 {
 	openssl rand -base64 45 | tr -d /=+ | cut -c -30
@@ -12,12 +12,12 @@ DATABASE_NAME="dbname"
 DATABASE_USER="dbuser"
 USER_PASSWORD=$(gen_pass)
 ADMIN_PASSWORD=$(gen_pass)
-DEVISE_PASSWORD=$(gen_pass)
+DEVISE_KEY_BASE=$(gen_pass)
+SECRET_KEY_BASE=$(gen_pass)
 
 mysql -v -u root -p -e"SET @dbname='$DATABASE_NAME';SET @username='$DATABASE_USER';"\
 "SET @user_password='$USER_PASSWORD';SET @admin_password='$ADMIN_PASSWORD';"\
 " `cat rails_bootstrap.sql`"
 
-#printf "[uwsgi]\nenv = ARDUBOY_DATABASE_PASSWORD=$USER_PASSWORD\n" > arduboy_spree.ini
-#printf "passwords:\n  user_pass: $USER_PASSWORD\n  admin_pass: $ADMIN_PASSWORD\n"\
-#"  devise_password: $DEVISE_PASSWORD\n" > database.yml
+printf "passwords:\n  user_pass: $USER_PASSWORD\n  admin_pass: $ADMIN_PASSWORD\n"\
+"  devise_key_base: $DEVISE_KEY_BASE\n  secret_key_base: $SECRET_KEY_BASE\n" > database.yml
