2dc3c986a84b7bbb70e31dca539eb2e80e6ec3d3
rogosher <shogerr@oregonstate.edu>
2016-01-05 18:34:33 -0800

    Create puppet_vagrant_bootstrap.sh

---
+++ virt/debian/bootstrap/puppet_vagrant_bootstrap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                                                    
+
+mkdir -p ./puppet/modules ./puppet/manifests ./puppet/hieradata
+touch ./puppet/manifests/default.pp                            
+cat > hiera.yaml <<EOL                                         
+---                                                            
+:backends:                                                     
+  - yaml                                                       
+:yaml:                                                         
+  :datadir: "hieradata"                                        
+:hierarchy:                                                    
+  - "node/%{::hostname}"                                       
+  - common                                                     
+EOL                                                            
