c0a3d9771c165593e7e94cf32ebc21d1c37abfaf
rogosher <shogerr@oregonstate.edu>
2016-02-06 00:52:59 -0800

    Update puppet_vagrant_bootstrap.sh

--- virt/debian/bootstrap/puppet_vagrant_bootstrap.sh
+++ virt/debian/bootstrap/puppet_vagrant_bootstrap.sh
@@ -1,14 +1,14 @@
 #!/bin/bash                                                    
 
 mkdir -p ./puppet/modules ./puppet/manifests ./puppet/hieradata
-touch ./puppet/manifests/default.pp                            
-cat > hiera.yaml <<EOF                                         
----                                                            
-:backends:                                                     
-  - yaml                                                       
-:yaml:                                                         
-  :datadir: "hieradata"                                        
-:hierarchy:                                                    
-  - "node/%{::hostname}"                                       
-  - common                                                     
+touch ./puppet/manifests/default.pp
+cat > hiera.yaml <<EOF
+---
+:backends:
+  - yaml
+:yaml:
+  :datadir: "hieradata"
+:hierarchy:
+  - "node/%{::hostname}"
+  - common
 EOF
