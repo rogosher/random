8155bb37ca5c037d27c4bef7a018b6e8fd6d2331
rogosher <shogerr@oregonstate.edu>
2016-02-06 04:55:23 -0800

    Update puppet_bootstrap.sh

--- virt/debian/bootstrap/puppet_bootstrap.sh
+++ virt/debian/bootstrap/puppet_bootstrap.sh
@@ -6,7 +6,7 @@ wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
 sudo dpkg -i puppetlabs-release-trusty.deb
 sudo apt-get update
 
-rm puppetlabs-release-trusty.deb
-sudo apt-get update
+sudo apt-get install puppet
 
+sudo apt-get update
 sudo puppet resource package puppet ensure=latest
